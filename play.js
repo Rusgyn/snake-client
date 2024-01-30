e4bf5a31777e50a846c28712fddf12b691e3246c
rusgyn <rusgynmagsanay@gmail.com>
2024-01-30 00:24:47 -0500

    setup stdin

--- play.js
+++ play.js
@@ -5,3 +5,23 @@ const { connect } = require("./client");
 connect();
 
 console.log("Connecting ...");
+//
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  const handleUserInput = function (key) {
+    // your code here
+    process.stdout.write('.');
+    // \u0003 maps to ctrl+c input
+    if (key === '\u0003') {
+      process.exit();
+    }
+  };
+
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+ setupInput()
