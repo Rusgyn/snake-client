6b6ca1fdbc2bd110b36f1d2f4ab7ef8f02869863
rusgyn <rusgynmagsanay@gmail.com>
2024-01-30 00:53:22 -0500

    removed commented out practice line of codes.

--- client.js
+++ client.js
@@ -2,7 +2,7 @@ const net = require('net');
 
 const connect = function() {
   const conn = net.createConnection({
-    host: "192.168.2.17", //you may confirm once server is up
+    host: "localhost", //you may confirm once server is up
     port: 50541 
   });
 
@@ -18,19 +18,6 @@ const connect = function() {
     conn.write("Name: RLM"); //this popup in the server once connection is established.
     // conn.write("Move: up");
   });
-
-  // /* Below (commented out) is the move command experiment */
-  //  conn.on("connect", () => {
-  //   let time = 1000;
-  //   const movement = ["Move: up", "Move: left", "Move: up", "Move: right", "Move: right", "Move: down", "Move: down", "Move: right"];
-  //   for (let command of movement) {
-  //     setTimeout(() => {
-  //       conn.write(command); //Transmitting data to server, each element of movement array.
-  //     }, time);
-  //     time += 1000; //incrementing the initial time per loop.
-  //   }
-    
-  // });
 
   conn.setEncoding("utf8"); // interpret incoming data as text
 
