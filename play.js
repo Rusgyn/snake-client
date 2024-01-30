6acc9ef2c40e7030fa3d991d6378469d7d39671b
rusgyn <rusgynmagsanay@gmail.com>
2024-01-30 00:55:42 -0500

    modularized

--- play.js
+++ play.js
@@ -2,26 +2,9 @@
 const net = require('net');
 
 const { connect } = require("./client");
-connect();
+const { setupInput } = require("./input");
 
 console.log("Connecting ...");
-//
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
+connect();
 
-  const handleUserInput = function (key) {
-    // your code here
-    process.stdout.write('.');
-    // \u0003 maps to ctrl+c input
-    if (key === '\u0003') {
-      process.exit();
-    }
-  };
-
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
- setupInput()
+setupInput();
