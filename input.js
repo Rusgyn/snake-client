2e3f169d2b14b9b147e891c37ced74359396cea6
rusgyn <rusgynmagsanay@gmail.com>
2024-01-30 10:33:48 -0500

    Transmit movement command to the server.

--- input.js
+++ input.js
@@ -1,16 +1,30 @@
 const net = require('net');
 
-const setupInput = function () {
+// Stores the active TCP connection object.
+let connection;
+
+const setupInput = (conn) => {
+
+  connection = conn;
+
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
-
+  
   const handleUserInput = function (key) {
     // on any input from stdin, output a "." to stdout
     process.stdout.write('.');
-    // \u0003 maps to ctrl+c input
-    if (key === '\u0003') {
+    
+    if (key === 'w') {
+      connection.write("Move: up")
+    } else if (key === 'a') {
+      connection.write("Move: left");
+    } else if (key === 's') {
+      connection.write("Move: down");
+    } else if (key === 'd') {
+      connection.write("Move: right");
+    } else if (key === '\u0003') { // \u0003 maps to ctrl+c input
       process.exit();
     }
   };
@@ -18,7 +32,7 @@ const setupInput = function () {
   stdin.on("data", handleUserInput);
   return stdin;
 };
- 
+
  module.exports = {
   setupInput,
  }
