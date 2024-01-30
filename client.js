f922150f5c1c59a3b4a1a0b801600dd8158c958d
rusgyn <rusgynmagsanay@gmail.com>
2024-01-30 00:26:21 -0500

    test and commented out Move command

--- client.js
+++ client.js
@@ -8,21 +8,35 @@ const connect = function() {
 
   //Receiving data from Server, once connection is established.
   conn.on("data", (data) => {
-    console.log("Server says: ", data); //
+    console.log("Server says: ", data);
   });
 
   conn.on("connect", () => {
-    console.log("Successfully connected to game server"); //Player to see.
+    console.log("Successfully connected to game server"); //Client/Player to see.
+
+    //Transmitting data to the Server
     conn.write("Name: RLM"); //this popup in the server once connection is established.
+    // conn.write("Move: up");
   });
 
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
+  // /* Below (commented out) is the move command experiment */
+  //  conn.on("connect", () => {
+  //   let time = 1000;
+  //   const movement = ["Move: up", "Move: left", "Move: up", "Move: right", "Move: right", "Move: down", "Move: down", "Move: right"];
+  //   for (let command of movement) {
+  //     setTimeout(() => {
+  //       conn.write(command); //Transmitting data to server, each element of movement array.
+  //     }, time);
+  //     time += 1000; //incrementing the initial time per loop.
+  //   }
+    
+  // });
+
+  conn.setEncoding("utf8"); // interpret incoming data as text
 
   return conn;
   
 };
-
 
 module.exports = {
   connect
