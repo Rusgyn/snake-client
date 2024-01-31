17c3a94cc74479d5ff5f7213acb83df9ab3538af
rusgyn <rusgynmagsanay@gmail.com>
2024-01-30 22:52:43 -0500

    saved constant values and exported to file/s where needed

---
+++ constants.js
@@ -0,0 +1,40 @@
+//This file contains the constants in this project.
+const net = require('net');
+
+//Connection details
+const IP = "192.168.2.17";
+const PORT = 50541;
+
+//Player detail
+const NAME = "Name: RLM";
+
+//use for snake movement
+const UP = "Move: up";
+const LEFT = "Move: left";
+const DOWN = "Move: down";
+const RIGHT = "Move: right";
+
+//use for snake canned messages
+const HI = "Say: Hi!";
+const ENJOY = "Say: Enjoy";
+const OK = "Say: Ok";
+const BYE = "Say: Bye";
+
+const keys = {
+  //snake movement
+  'w': UP,
+  'a': LEFT,
+  's': DOWN,
+  'd': RIGHT,
+  //canned messages
+  'z': HI,
+  'x': ENJOY,
+  'c': OK,
+  'q': BYE
+}
+//To obtain the object keys key property to be compared.
+const char = Object.keys(keys);
+
+module.exports = {
+  IP, PORT, NAME, UP, LEFT, DOWN, RIGHT, HI, ENJOY, OK, BYE, keys, char
+};
