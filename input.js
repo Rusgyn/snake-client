febc954805facdd41ff43848c58d3ade9e238c29
rusgyn <rusgynmagsanay@gmail.com>
2024-01-31 15:27:46 -0500

    Refactor. Separate the two functions handleUserInput() and setupInput().

--- input.js
+++ input.js
@@ -1,25 +1,26 @@
 const { keys, char } = require("./constants");
+let connection;
 
-const setupInput = (connection) => {
-
+const setupInput = (conn) => {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
-  
-  const handleUserInput = function(key) {
-    if (key === '\u0003') { // \u0003 maps to ctrl+c input
-      process.stdout.write('\nThank you for playing!\n');
-      process.exit();
-    } else if (!char.includes(key)) {
-      process.stdout.write('.'); //output a "." to stdout
-    } else if (char.includes(key)) {
-      connection.write(keys[key.toLowerCase()]);
-    }
-  };
-
   stdin.on("data", handleUserInput);
+
   return stdin;
+};
+
+const handleUserInput = function(key) {
+  if (key === '\u0003') { // \u0003 maps to ctrl+c input
+    process.stdout.write('\nThank you for playing!\n');
+    process.exit();
+  } else if (!char.includes(key)) {
+    process.stdout.write('.'); //output a "." to stdout
+  } else if (char.includes(key)) {
+    connection.write(keys[key.toLowerCase()]);
+  }
 };
 
 module.exports = { setupInput };
