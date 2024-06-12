4b3ad01d4dcb531931acfec8665ca7dc13d8016a
rusgyn <rusgynmagsanay@gmail.com>
2024-06-12 16:57:37 -0400

    Implement functions that handle user input from stdin.

--- play.js
+++ play.js
@@ -3,4 +3,26 @@
 const connect = require('./client'); // Import the function to use.
 
 console.log("Connecting ... ");
+
+//sertupInpute() and handleUserInput() - establish our UI (user interface) where players can send keyboard commands which we will soon use to control the snake.
+
+// setup interface to handle user input from stdin
+const setupInput = function() {
+  const stdin = process.stdin; // create variable to hold the stdin object so we don't have to type process.stdin multiple times
+
+  stdin.setRawMode(true); // // Raw Mode allows us to listen for individual keypresses instead of waiting for the user to press enter
+  stdin.setEncoding("utf-8"); // interpret data to text
+  stdin.resume(); // resume stdin so that the program can listen
+  stdin.on("data", handleUserInput);
+  
+  return stdin; // return the stdin object so we can it elsewhere in the program.
+};
+
+const handleUserInput = function (key) {
+  if (key === "\u0003") { //Key terminates the game session using `ctrl` + `c`
+    process.exit();
+  }
+};
+
 connect(); // calling the connect function that is being imported.
+setupInput(); // calling the setUpInput().
