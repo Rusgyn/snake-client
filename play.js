cfb8cfca22dbcbb745ac674c794d07e67283a460
rusgyn <rusgynmagsanay@gmail.com>
2024-06-12 15:02:17 -0400

    Refactor the code and separate the functions, import for connectivity.

--- play.js
+++ play.js
@@ -1,26 +1,6 @@
-// Script to establish a TCP connection and log incoming messages.
+// Script that calls our function that establishes connection to the server.
 
-const net = require("net"); // net allows our node apps to use TCP.
-let port = 50541;
-
-// Establishes a connection to our intent game server
-const connect = function() {
-  
-  const conn = net.createConnection({
-    host: "localhost",
-    port: port,
-  });
-
-  // Interpret incoming data as text
-  conn.setEncoding("utf-8");
-
-  // Handle incoming messages/data from the server.
-  conn.on("data", (data) => {
-    console.log("Snake Host says: ", data);
-  });
-
-  return conn;
-};
+const connect = require('./client'); // Import the function to use.
 
 console.log("Connecting ... ");
 connect();
