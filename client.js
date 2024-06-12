bff96bff8d3606c218f4db7bc066dd3edb5439f0
rusgyn <rusgynmagsanay@gmail.com>
2024-06-12 15:03:29 -0400

    Implement new script, modularize the connect function. Export back to client page play.js

---
+++ client.js
@@ -0,0 +1,26 @@
+// Connection Module. Script to establish a TCP connection and log incoming messages.
+
+const net = require("net"); // net allows our node apps to use TCP.
+
+let port = 50541;
+
+// Establishes a connection to our intent game server
+const connect = function() {
+  
+  const conn = net.createConnection({
+    host: "localhost",
+    port: port,
+  });
+
+  // Interpret incoming data as text
+  conn.setEncoding("utf-8");
+
+  // Handle incoming messages/data from the server.
+  conn.on("data", (data) => {
+    console.log("Snake Host says: ", data);
+  });
+
+  return conn;
+};
+
+module.exports = connect;
